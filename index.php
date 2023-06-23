9d9ad6455876135295c7f86af6e341c1d6f85347
Santality <saintly231@gmail.com>
2023-06-23 23:34:37 +0500

    11

--- index.php
+++ index.php
@@ -52,6 +52,19 @@
                 </div>
             </div>
         </div>
+        <h1 class="catalogue_title">Каталог</h1>
+        <div class="catalogue">
+            <div class="catalogue_card">
+                <div class="catalogue_img">
+                    <img src="/img/309d3d87-10bf-492d-8ac3-b39efca69cd3.jpg" alt="">
+                </div>
+                <p class="catalogue_card_name">Кофе "Jacobs"</p>
+                <div class="catalogue_card_cb">
+                    <p class="catalogue_card_cost">500₽</p>
+                    <a class="catalogue_card_btn" href="/product.php"><img src="/img/cart.svg" alt="cart.svg"></a>
+                </div>
+            </div>
+        </div>
     </div>
 
     <script>
