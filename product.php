169bfa605c5494219d4473fb543e892fc351eb76
XopK <dmahmutov12@gmail.com>
2023-06-24 03:10:06 +0500

    gfgfg

--- product.php
+++ product.php
@@ -15,18 +15,18 @@
     require_once "connect.php";
     ?>
     <?php
-    $id = !empty($_GET['id_catalogue'])?$_GET['id_catalogue']:false;
+    $id = !empty($_GET['id_catalogue']) ? $_GET['id_catalogue'] : false;
     $product_select = "SELECT id_product, users.name AS name_user, product.name AS product_name, photo_product, description, amount, cost, date_added, id_seller, surname FROM `product` JOIN `users` ON `users`.`id_user` = `product`.`id_seller` WHERE id_product = '$id'";
-    $select_result = mysqli_fetch_array(mysqli_query($con,$product_select));
+    $select_result = mysqli_fetch_array(mysqli_query($con, $product_select));
     ?>
     <div class="main_frame">
         <div class="product_frame">
             <div class="product_frame_left">
-                <img src="/img/<?=$select_result['photo_product']?>" alt="<?=$select_result['photo_product']?>">
+                <img src="/img/<?= $select_result['photo_product'] ?>" alt="<?= $select_result['photo_product'] ?>">
             </div>
             <div class="product_frame_right">
-                <p class="product_frame_title"><?=$select_result['product_name']?></p>
-                <p class="product_frame_cost"><?=$select_result['cost']?>₽</p>
+                <p class="product_frame_title"><?= $select_result['product_name'] ?></p>
+                <p class="product_frame_cost"><?= $select_result['cost'] ?>₽</p>
                 <div class="count">
                     <span class="change minus min">
                         <img src="/img/minus.png" alt="minus.png">
@@ -35,7 +35,7 @@
                     <span class="change plus">
                         <img src="/img/plus.png" alt="plus.png">
                     </span>
-                    <span class="count_end">Количество: <?=$select_result['amount']?></span>
+                    <span class="count_end">Количество: <?= $select_result['amount'] ?></span>
                 </div>
                 <div class="product_frame_but">
                     <button class="product_but_left">В корзину</button>
@@ -44,39 +44,50 @@
                     <div class="product_seller_photo"><img src="/img/user.png" alt=""></div>
                     <div>
                         <p>Продавец:</p>
-                        <p><?=$select_result['name_user']?> <?=$select_result['surname']?></p>
+                        <p><?= $select_result['name_user'] ?> <?= $select_result['surname'] ?></p>
                     </div>
                 </div>
             </div>
         </div>
         <div class="product_description_block">
             <h1 class="product_description_title">Описание</h1>
-            <p class="product_description"><?=$select_result['description']?></p>
+            <p class="product_description"><?= $select_result['description'] ?></p>
         </div>
         <?php
         include "connect.php";
-        $query = "select * from review";
+        $query_review = "SELECT * FROM `review` JOIN `users` ON review.user = users.id_user WHERE product = '$id' ORDER BY date_review DESC";
+        $result_review = mysqli_query($con, $query_review);
         ?>
         <div class="product_reviews_block">
             <h1 class="product_reviews_title">Отзывы</h1>
             <? if (isset($_SESSION['id_user'])) { ?>
-                <form action="review.php">
+                <form action="/reviewDB.php" method="post">
                     <div class="review_answer">
-                        <input placeholder="Оставьте отзыв!" type="text">
+                        <input type="hidden" value="<?=$id?>" name="id_rev">
+                        <input placeholder="Оставьте отзыв!" type="text" name="comment">
                         <button><img src="/img/plus-white.svg" alt="plus-white.svg"></button>
                     </div>
                 </form>
             <? } ?>
-            <div class="review_block">
-                <div class="review_img"><img src="/img/user.png" alt=""></div>
-                <div class="review_block_text">
-                    <h1 class="review_name">Иванов И.</h1>
-                    <p class="review_text">Очень вкусный кофе. Доставили быстро</p>
+            <?
+            while ($review_info = mysqli_fetch_array($result_review)) {
+                $dateB = $review_info['date_review'];
+                $newDate = date("d.m.Y", strtotime($dateB));
+            ?>
+                <div class="review_block">
+                    <div class="review_img"><img src="/img/user.png" alt=""></div>
+                    <div class="review_block_text">
+                        <h1 class="review_name"><?=$review_info['name']?> <?=$review_info['surname']?></h1>
+                        <p class="review_text"><?=$review_info['comment']?></p>
+                    </div>
+                    <div class="review_date">
+                        <p><?=$newDate?></p>
+                    </div>
                 </div>
-                <div class="review_date">
-                    <p>11.09.2001</p>
-                </div>
-            </div>
+            <?
+            }
+            ?>
+
         </div>
     </div>
     <script>
--- reviewDB.php
+++ reviewDB.php
@@ -1,3 +1,21 @@
 <?php
-include "connect.php";
+session_start();
+require_once "connect.php";
+if(!empty($_POST)){
+    $id_use = $_SESSION['id_user'];
+    $id_prod = $_POST['id_rev'];
+    $comm = $_POST['comment'];
+    $date_review = date("d.m.Y");
+
+    $review_add = "INSERT INTO `review`(`id_review`, `comment`, `product`, `user`, `date_review`) VALUES (null,'$comm','$id_prod','$id_use','$date_review')";
+    $result = mysqli_query($con,$review_add);
+
+    if ($result) {
+        echo "<script>alert('Успех'); location.href = '/product.php';</script>";
+    } else {
+        echo "<script>alert('Ошибка');</script>";
+        echo mysqli_error($con);
+    }
+}
+
 ?>
