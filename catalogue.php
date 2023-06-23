7ec380a9063ab410c161a5eaaeb053d60dc44505
Santality <saintly231@gmail.com>
2023-06-23 23:24:05 +0500

    5

---
+++ catalogue.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="shortcut icon" href="/img/icon.ico" type="image/x-icon">
+    <title>Каталог</title>
+</head>
+<body>
+    <?include "header.php"?>
+    <div class="main_frame">
+        <h1 class="catalogue_title">Каталог</h1>
+        <div class="catalogue">
+            <div class="catalogue_card">
+                <div class="catalogue_img">
+                    <img src="/img/309d3d87-10bf-492d-8ac3-b39efca69cd3.jpg" alt="">
+                </div>
+                <p class="catalogue_card_name">Кофе "Jacobs"</p>
+                <div class="catalogue_card_cb">
+                    <p class="catalogue_card_cost">500₽</p>
+                    <a class="catalogue_card_btn" href="/product.php"><img src="/img/cart.svg" alt="cart.svg"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/itc-slider.css">
+    <link rel="shortcut icon" href="/img/icon.ico" type="image/x-icon">
     <script defer src="/js/itc-slider.js"></script>
     <link rel="stylesheet" href="/css/style.css">
     <title>Maureen</title>
@@ -48,19 +49,6 @@
                         <button class="itc-slider-btn itc-slider-btn-prev"></button>
                         <button class="itc-slider-btn itc-slider-btn-next"></button>
                     </div>
-                </div>
-            </div>
-        </div>
-        <h1 class="catalogue_title">Каталог</h1>
-        <div class="catalogue">
-            <div class="catalogue_card">
-                <div class="catalogue_img">
-                    <img src="/img/309d3d87-10bf-492d-8ac3-b39efca69cd3.jpg" alt="">
-                </div>
-                <p class="catalogue_card_name">Кофе "Jacobs"</p>
-                <div class="catalogue_card_cb">
-                    <p class="catalogue_card_cost">500₽</p>
-                    <a class="catalogue_card_btn" href="/product.php"><img src="/img/cart.svg" alt="cart.svg"></a>
                 </div>
             </div>
         </div>
